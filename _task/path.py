c0d098174ec395134ac3ff179125f57de2e33fbb
FrierenZk <lingfengfroze@yahoo.com>
2020-06-22 19:27:28 +0800

    Fix bugs

--- _task/path.py
+++ _task/path.py
@@ -2,9 +2,14 @@ import os
 
 from _task.config import config
 
-type_dir = {
+server_dir = {
     "trunk": "maintrunk",
-    "branches": "branches_version"
+    "branches": "branches_version",
+    "tags": "tags_version"
+}
+
+type_dir = {
+    "trunk": "maintrunk"
 }
 
 
@@ -13,21 +18,21 @@ def _server_dir(task: str):
     t = config.get_type(task)
     if t is None:
         return None
-    if t in type_dir.keys():
-        type_path = type_dir[t]
-    else:
-        type_path = t
-    return ("172.18.36.250:/volume1/version/" + type_path + "/" + task + "/").strip()
+    if t in server_dir.keys():
+        t = server_dir[t]
+    return ("172.18.36.250:/volume1/version/" + t + "/" + task + "/").strip()
 
 
 def _image_dir(task: str):
     t = config.get_type(task)
     if t is None:
         return None
+    if t in type_dir.keys():
+        t = type_dir[t]
     name = config.get_name(task)
     if name is None:
         return None
-    return os.path.expanduser('~')+"/catv/" + t + "/" + name + "/"
+    return os.path.expanduser('~') + "/catv/" + t + "/" + name + "/"
 
 
 def _source_dir(task: str):
