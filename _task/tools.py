57aa07caa90c9636c0e50fe9efc6498e30433c67
FrierenZk <lingfengfroze@yahoo.com>
2020-06-22 13:51:25 +0800

    Add profiles

--- _task/tools.py
+++ _task/tools.py
@@ -9,12 +9,28 @@ x = {
         "fujian": {
             "name": "fujian",
             "profile": "catv_fujian_nocolor_hgu_xpon_wifi_cable_voice_usb"
+        },
+        "wisecable": {
+            "name": "wisecable",
+            "profile": "catv_wisecable_nocolor_hgu_xpon_wifi_nocable_novoice_nousb"
+        },
+        "hgustandard": {
+            "name": "hgustandard",
+            "profile": "catv_standard_nocolor_hgu_xpon_wifi_nocable_novoice_nousb"
+        },
+        "sfustandard": {
+            "name": "sfustandard",
+            "profile": "catv_standard_nocolor_sfu_xpon_nowifi_nocable_novoice_nousb"
         }
     },
     "tags": {
         "armenia": {
             "name": "armenia-1.0",
             "profile": "catv_armenia_nocolor_hgu_xpon_wifi_nocable_novoice_nousb"
+        },
+        "guangxi": {
+            "name": "guangxi-1.0",
+            "profile": "catv_guangxi_nocolor_hgu_xpon_wifi_cable_voice_usb"
         }
     }
 }
