cf335b158a68973ec1f24da4abcd2444b6ce23b6
FrierenZk <lingfengfroze@yahoo.com>
2020-06-23 11:27:06 +0800

    Fix a bug

--- linuxBuilder.sh
+++ linuxBuilder.sh
@@ -1,3 +1,3 @@
 python3 ./_task/tools.py
 pyinstaller -D --clean __main__.py -n Muppet-Python-x86_64-linux
-mv ./_task/build_list.json ./dist/Muppet-Python-x86_64-linux/build_list.json
+mv ./build_list.json ./dist/Muppet-Python-x86_64-linux/build_list.json
