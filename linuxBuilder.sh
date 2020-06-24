feb92e54b75c5e5be21dd8d94edcc97ee5be56ae
FrierenZk <lingfengfroze@yahoo.com>
2020-06-24 15:37:26 +0800

    Slient build

--- linuxBuilder.sh
+++ linuxBuilder.sh
@@ -1,3 +1,3 @@
 python3 ./_task/tools.py
-pyinstaller -D --clean __main__.py -n Muppet-Python-x86_64-linux
+pyinstaller -D --clean __main__.py -n Muppet-Python-x86_64-linux -y
 mv ./build_list.json ./dist/Muppet-Python-x86_64-linux/build_list.json
