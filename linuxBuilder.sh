2389484b25b44c464f73582bf4ef0d84b45d6cf0
FrierenZk <lingfengfroze@yahoo.com>
2020-06-29 20:12:15 +0800

    Modify linuxBuilder.sh

--- linuxBuilder.sh
+++ linuxBuilder.sh
@@ -1,3 +1,3 @@
-python3 ./_task/tools.py
+python3 ./tools.py
 pyinstaller -D --clean __main__.py -n Muppet-Python-x86_64-linux -y
 mv ./build_list.json ./dist/Muppet-Python-x86_64-linux/build_list.json
