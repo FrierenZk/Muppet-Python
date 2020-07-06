7188cd6ba88c0c90ce06d4d9e8a90a309d513e90
FrierenZk <lingfengfroze@yahoo.com>
2020-07-06 19:20:31 +0800

    Use multiprocessing.process instead of threading.thread

--- _task/task_entity.py
+++ _task/task_entity.py
@@ -1,131 +1,50 @@
-from os import chdir, listdir, remove
-from os.path import join, isfile, isdir, getsize
-from shutil import rmtree
-from subprocess import PIPE, Popen, run
 from threading import Thread
-
-from _task.config import config
-from _task.path import _server_dir, _source_dir, _image_dir
+from multiprocessing import Value
+from ctypes import c_wchar_p
 
 
 class TaskEntity:
     class TaskThread(Thread):
-        shell_process: Popen = None
+        process = None
 
         def run(self) -> None:
             try:
-                chdir(_source_dir(self.task))
-                if config.get_update(self.task) != False:
-                    if config.get_cleanup(self.task):
-                        cmd = "svn cleanup "
-                        if config.get_cleanupSudo(self.task):
-                            cmd = "sudo "+cmd
-                        ret = run(cmd + config.get_cleanupPath(self.task), shell=True)
-                        print("task=" + self.task, "svn cleanup", ret)
-                    ret = run("svn up", shell=True)
-                    print("task=" + self.task, "svn up", ret)
-                else:
-                    print("svn up skipped")
-                cmd = "./mkfw.sh " + self.profile
-                cmd = cmd + " clean && " + cmd
-                self._image_clean()
-                print("making compilation...")
-                self.shell_process = Popen(cmd, shell=True, bufsize=30, stdout=PIPE,
-                                           stderr=PIPE)
-                out, err = self.shell_process.communicate()
-                self.shell_process.wait()
-                if self.shell_process.returncode == 0:
-                    print(cmd, "success")
-                    if config.get_upload(self.task):
-                        self._upload_image()
-                    else:
-                        print(self.task, "finished with no uploaded")
-                else:
-                    print("task=" + self.task, "cmd=" + cmd, "cmd failed")
-                    print(out)
-                    print(err)
-            except Exception as err:
-                print(self.task, err)
-            finally:
-                self._callback(self.task)
-
-        def __init__(self, task: str, profile: str, callback):
-            super().__init__()
-            self.task = task
-            self.profile = profile
-            self.image_dir = _image_dir(task)
-            self._callback = callback
-
-        def _upload_image(self):
-            file_path = ""
-            files = listdir(self.image_dir)
-            for file in files:
-                if file.find(".tar.gz") == -1:
-                    continue
-                file_path = self.image_dir + file
-                break
-            if file_path != "":
-                if getsize(file_path) < (1024 * 1024):
-                    print(file_path, "size is not right, upload canceled")
-                    return
-                ret = run(
-                    "sudo sshpass -p 654321 scp " + file_path + " buildmanager@" + _server_dir(self.task), shell=True)
-                if ret.returncode == 0:
-                    print("task=" + self.task, "upload success")
-                else:
-                    print("task=" + self.task, "upload fail", ret)
-            else:
-                print("image not exist")
-
-        def _image_clean(self):
-            print("image cleaning at ", self.image_dir)
-            try:
-                files = listdir(self.image_dir)
-                for file in files:
-                    target = join(self.image_dir, file)
-                    print("delete ", target)
-                    if isfile(target):
-                        remove(join(self.image_dir, file))
-                    elif isdir(target):
-                        rmtree(target)
+                from _task.task_process import TaskProcess
+                self.process = TaskProcess(self.task)
+                self.process.daemon = True
+                self.process.start()
+                while self.process.is_alive():
+                    self.process.join(10)
             except Exception as err:
                 print(err)
+            finally:
+                self._callback(self.task)
+                print("task", self.task, "thread terminated")
+
+        def __init__(self, task: str, callback):
+            super().__init__()
+            self.task: Value = Value(c_wchar_p, task)
+            self._callback = callback
+
+        def terminate(self):
+            if self.process is not None:
+                if self.process.is_alive():
+                    print("task", self.task, "terminating")
+                    self.process.terminate()
 
     task: str
-    _profile: str
-    valid = False
     _task: TaskThread = None
 
     def __init__(self, task: str, callback):
         self.task = task
-        self._profile = config.get_profile(task)
         self._callback = callback
-        if (task is not None) & (self._profile is not None):
-            self.valid = True
 
     def run(self):
-        if self.valid:
-            self._task = self.TaskThread(self.task, self._profile, self._callback)
-            self._task.start()
-        else:
-            self._callback(self.task)
-            if self.task is None:
-                self.task = "None"
-            if self._profile is None:
-                self._profile = "None"
-            print("task invalid", "task=" + self.task, "profile=" + self._profile)
+        self._task = self.TaskThread(self.task, self._callback)
+        self._task.start()
 
     def terminate(self):
         if self._task is None:
             return
-        if self._task.shell_process.poll() is None:
-            self._task.shell_process.terminate()
-        print("task", self.task, "terminated")
+        self._task.terminate()
         return
-
-    def __del__(self):
-        if self._task is None:
-            return
-        if self._task.shell_process is None:
-            return
-        self._task.shell_process.kill()
---
+++ _task/task_process.py
@@ -0,0 +1,97 @@
+from multiprocessing import Process, Value
+from os import chdir, remove, listdir
+from os.path import isfile, isdir, join, getsize
+from shutil import rmtree
+from subprocess import run, Popen, PIPE
+
+from _task import _source_dir, _image_dir, _server_dir
+from _task.config import config
+
+
+class TaskProcess(Process):
+    task: str
+
+    def __init__(self, task: Value):
+        super().__init__()
+        self.task = task.value
+
+    def run(self) -> None:
+        try:
+            if config.get_update(self.task) is not False:
+                self._svn_update()
+            else:
+                print("svn up skipped")
+            self._image_clean()
+            ret: bool = self._image_build()
+            if ret & config.get_upload(self.task) is True:
+                self._image_upload()
+            else:
+                print(self.task, "finished with no uploaded")
+        except Exception as err:
+            print(err)
+
+    def _svn_update(self):
+        chdir(_source_dir(self.task))
+        if config.get_cleanup(self.task):
+            cmd = "svn cleanup "
+            if config.get_cleanupSudo(self.task):
+                cmd = "sudo " + cmd
+            ret = run(cmd + config.get_cleanupPath(self.task), shell=True)
+            print("task=" + self.task, "svn cleanup", ret)
+        ret = run("svn up", shell=True)
+        print("task=" + self.task, "svn up", ret)
+
+    def _image_clean(self):
+        image_dir = _image_dir(self.task)
+        print("image cleaning at ", image_dir)
+        try:
+            files = listdir(image_dir)
+            for file in files:
+                target = join(image_dir, file)
+                print("delete ", target)
+                if isfile(target):
+                    remove(join(image_dir, file))
+                elif isdir(target):
+                    rmtree(target)
+        except Exception as err:
+            print(err)
+
+    def _image_build(self):
+        chdir(_source_dir(self.task))
+        cmd = "./mkfw.sh " + config.get_profile(self.task)
+        cmd = cmd + " clean && " + cmd
+        print("making compilation...")
+        shell_process = Popen(cmd, shell=True, bufsize=30, stdout=PIPE,
+                              stderr=PIPE)
+        out, err = shell_process.communicate()
+        shell_process.wait()
+        if shell_process.returncode == 0:
+            print(cmd, "success")
+            return True
+        else:
+            print("task=" + self.task, "cmd=" + cmd, "cmd failed")
+            print(out)
+            print(err)
+            return False
+
+    def _image_upload(self):
+        image_dir = _image_dir(self.task)
+        file_path = ""
+        files = listdir(image_dir)
+        for file in files:
+            if file.find(".tar.gz") == -1:
+                continue
+            file_path = image_dir + file
+            break
+        if file_path != "":
+            if getsize(file_path) < (1024 * 1024):
+                print(file_path, "size is not right, upload canceled")
+                return
+            ret = run(
+                "sudo sshpass -p 654321 scp " + file_path + " buildmanager@" + _server_dir(self.task), shell=True)
+            if ret.returncode == 0:
+                print("task=" + self.task, "upload success")
+            else:
+                print("task=" + self.task, "upload fail", ret)
+        else:
+            print("image not exist")
