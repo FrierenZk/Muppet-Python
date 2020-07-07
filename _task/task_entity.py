b8e20214b1d33a624a3c8f7b998e5bbc76d6b103
FrierenZk <lingfengfroze@yahoo.com>
2020-07-07 10:19:55 +0800

    Reconstruct with Process

--- _task/task_entity.py
+++ _task/task_entity.py
@@ -1,50 +1,41 @@
-from threading import Thread
-from multiprocessing import Value
-from ctypes import c_wchar_p
+from ctypes import c_wchar_p, c_bool
+from multiprocessing import Process, Value
 
 
-class TaskEntity:
-    class TaskThread(Thread):
-        process = None
-
-        def run(self) -> None:
-            try:
-                from _task.task_process import TaskProcess
-                self.process = TaskProcess(self.task)
-                self.process.daemon = True
-                self.process.start()
-                while self.process.is_alive():
-                    self.process.join(10)
-            except Exception as err:
-                print(err)
-            finally:
-                self._callback(self.task)
-                print("task", self.task, "thread terminated")
-
-        def __init__(self, task: str, callback):
-            super().__init__()
-            self.task: Value = Value(c_wchar_p, task)
-            self._callback = callback
-
-        def terminate(self):
-            if self.process is not None:
-                if self.process.is_alive():
-                    print("task", self.task, "terminating")
-                    self.process.terminate()
-
+class TaskEntity(Process):
     task: str
-    _task: TaskThread = None
 
     def __init__(self, task: str, callback):
+        super().__init__()
         self.task = task
         self._callback = callback
+        self.unregistered = Value(c_bool, False)
+        from _task.task_process import TaskProcess
+        self.process = TaskProcess(Value(c_wchar_p, self.task))
 
-    def run(self):
-        self._task = self.TaskThread(self.task, self._callback)
-        self._task.start()
+    def run(self) -> None:
+        try:
+            self.process.daemon = True
+            self.process.start()
+            while self.process.is_alive():
+                self.process.join(10)
+        except Exception as err:
+            print(err)
+        finally:
+            self.unregister()
+            print("task", self.task, "thread terminated")
 
     def terminate(self):
-        if self._task is None:
-            return
-        self._task.terminate()
+        if self.process is not None:
+            if self.process.is_alive():
+                self.process.terminate()
+        self.unregister()
+        super(TaskEntity, self).terminate()
         return
+
+    def unregister(self):
+        self.unregistered.acquire()
+        if self.unregistered.value is False:
+            self._callback(task=self.task, flag=True)
+            self.unregistered.value = True
+        self.unregistered.release()
