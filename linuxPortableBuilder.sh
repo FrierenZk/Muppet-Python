8bc8b5461ce5cad69c822ef59f33e9ebdf3763ed
FrierenZk <FrierenZk@github.com>
2020-07-09 14:29:04 +0800

    Avoid file name collision

--- linuxPortableBuilder.sh
+++ linuxPortableBuilder.sh
@@ -1,7 +1,7 @@
 python3 ./tools.py
-pyinstaller -F --clean __main__.py -n Muppet-Python-Portable-x86_64-linux -y
+pyinstaller -F --clean __main__.py -n Muppet-Python-Portable-x86_64-linux.bin -y
 if [ ! -d ./dist/Muppet-Python-Portable-x86_64-linux  ];then
 	mkdir ./dist/Muppet-Python-Portable-x86_64-linux
 fi
 mv ./build_list.json ./dist/Muppet-Python-Portable-x86_64-linux/build_list.json
-mv ./dist/Muppet-Python-Portable-x86_64-linux ./dist/Muppet-Python-Portable-x86_64-linux/Muppet-Python-Portable-x86_64-linux
+mv ./dist/Muppet-Python-Portable-x86_64-linux.bin ./dist/Muppet-Python-Portable-x86_64-linux/Muppet-Python-Portable-x86_64-linux
