7379f6c125c7632245435ad2d8eee16160bd1d6f
FrierenZk <lingfengfroze@yahoo.com>
2020-08-21 14:18:23 +0800

    Fix bytes transformation

--- _task/task_thread.py
+++ _task/task_thread.py
@@ -72,19 +72,14 @@ class TaskThread(Thread):
             err = None
             try:
                 out, err = self.shell_process.communicate(timeout=1)
+                ret = self.shell_process.poll()
             except Exception as _:
                 # Do nothing
                 pass
             if out is not None:
-                self.push_with_print(str(out))
+                self.push_with_print(str(out, encoding='utf-8'))
             if err is not None:
-                self.push_with_print(str(err))
-            ret = self.shell_process.poll()
-        out, err = self.shell_process.communicate()
-        if out is not None:
-            self.push_with_print(str(out))
-        if err is not None:
-            self.push_with_print(str(err))
+                self.push_with_print(str(err, encoding='utf-8'))
         return ret
 
     def _svn_update(self) -> bool:
